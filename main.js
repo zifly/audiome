05ef25121c1899fd1e1e1e6fdfe1d0f171f74db0
zifly <yuyi.cd@gmail.com>
2018-10-09 22:02:07 +0800

    ipcmain: listening the channel add new book

--- main.js
+++ main.js
@@ -9,60 +9,37 @@ const spawn = require('child_process').spawn;
   
   function createWindow () {  
     electron.Menu.setApplicationMenu(null)    //hide electron Menu
-   
-    // 创建浏览器窗口
+
     win = new BrowserWindow({ width: 1024, height:760 })
-    win.openDevTools();
+    //win.openDevTools();
 
-    // 然后加载应用的 html.html 
-    //__dirname,表示main.js所在的目录路径
-
-    win.loadURL(__dirname + '/htmls/default.html')
+      win.loadURL(__dirname + '/htmls/default.html')
   }
   
   app.on('ready', createWindow);
 
-
-  ipc.on('audio-new',function(event,arg) {
-      console.log(arg);
-      nfile="./htmls/"+arg;
-      fs.readFile(nfile, 'utf-8', function (err, data) {
-        if (err) {
-           } 
-        else { 
-          event.sender.send('audio-new-reply',data)
-        }
-      });
-    
-  });
-
-  ipc.on('save-bookinfo',function(event,arg) {
-    console.log(arg);
-    runpath=process.cwd()+"\\scripts\\new-book.js";
-    console.log('runpath='+runpath)
+  //listening the channel create a new book and write info to json
+  ipc.on('new-bookinfo',function(event,arg) {
+   
+    runpath=process.cwd()+"\\scripts\\bookinfo.js";
     argstr=arg.join("**");
     var sendstr="OK";
 
-    const ls = spawn('node', [runpath, argstr]);
+    const ls = spawn('node', [runpath,1,argstr]);
       ls.stdout.on('data', (data) => {
         console.log(`${data}`);
-        if(data.indexOf('Error 9999:')>-1){
-          //datastr=data;
-          //estr=datastr.split('Error 9999:');
-          sendstr=data;
+        datastr=data.toString();
+        if(datastr.indexOf('ERROR 9999:')>-1){
+          sendstr=datastr;
         }
       });
 
       ls.stderr.on('data', (data) => {
-        
-        //sendstr=data;
-        //console.log(sendstr);
+         sendstr=data;
       });
 
       ls.on('close', (code) => {
-          //console.log(`child process exited with code ${code}`);
-          //console.log('errstr='+sendstr);
-          event.sender.send('save-bookinfo-reply',sendstr);
+           event.sender.send('save-bookinfo-reply',sendstr);
       });
       
   
